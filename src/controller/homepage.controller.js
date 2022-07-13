7b8be9a60cdeb84df32c116f23766abf0e25ca9a
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-07-13 19:49:01 -0300

    feat: Adicionando a classe que adiciona o modal de adicionar hábito e deixando funcional

--- src/controller/homepage.controller.js
+++ src/controller/homepage.controller.js
@@ -3,6 +3,14 @@ import Modal from "../models/modal.models.js";
 
 const responseUser = await Api.readAll();
 
+let maxHabits = 10;
+
+const orderedHabits = responseUser.sort((a, b) => {
+  return b.habit_id - a.habit_id;
+});
+
+console.log(orderedHabits);
+
 export default class Homepage {
   static getUser = JSON.parse(localStorage.getItem("@kenzie-habits:user"));
 
@@ -27,27 +35,24 @@ export default class Homepage {
   }
 
   static async createHabitCard(event) {
-    event.preventDefault();
-
     const objectValues = [...event.target];
 
     console.log(objectValues);
 
-    const btnCreateHabit = document.querySelector(
-      ".modalCreateHabitContentBtn"
-    );
+    const btnCreateHabit = objectValues[3];
 
     const objectForm = {
       habit_title: objectValues[0].value,
       habit_description: objectValues[1].value,
       habit_category: objectValues[2].value,
     };
-    console.log(objectForm);
+    await Api.createHabit(objectForm);
+    location.reload();
   }
 
-  static renderHabit(data) {
-    const ul = document.querySelector(".tableBody");
+  static ul = document.querySelector(".tableBody");
 
+  static renderHabit(data) {
     const li = document.createElement("li");
     const inputCheckbox = document.createElement("input");
     const labelTitle = document.createElement("label");
@@ -60,17 +65,42 @@ export default class Homepage {
     inputCheckbox.id = data.habit_id;
 
     inputCheckbox.type = "checkbox";
-    labelTitle.innerText = data.habit_title;
-    pDescription.innerText = data.habit_description;
-    spanCategory.innerText = data.habit_category;
+    labelTitle.innerText = data.habit_title.replace(
+      /^./,
+      data.habit_title[0].toUpperCase()
+    );
+    pDescription.innerText = data.habit_description.replace(
+      /^./,
+      data.habit_description[0].toUpperCase()
+    );
+    spanCategory.innerText = data.habit_category.replace(
+      /^./,
+      data.habit_category[0].toUpperCase()
+    );
+
     imgButton.src = "../assets/img/Button Options.png";
+
+    if (data.habit_category === "saude") {
+      spanCategory.innerText = "Saúde";
+    }
 
     btnEdit.appendChild(imgButton);
     li.append(inputCheckbox, labelTitle, pDescription, spanCategory, btnEdit);
-    ul.append(li);
+    this.ul.append(li);
   }
 }
 
-responseUser.map((elem) => {
+orderedHabits.slice(0, maxHabits).map((elem) => {
   Homepage.renderHabit(elem);
+});
+
+const btnMoreHabits = document.querySelector(".loadMore");
+
+btnMoreHabits.addEventListener("click", (event) => {
+  event.preventDefault();
+  Homepage.ul.innerHTML = "";
+  maxHabits += 5;
+  orderedHabits.slice(0, maxHabits).map((elem) => {
+    Homepage.renderHabit(elem);
+  });
 });
