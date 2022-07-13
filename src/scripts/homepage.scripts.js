2994a0aff2c49346fcedc6442840caba2ae8eaba
Leonardo Rodrigues <leonardorodriguesaai@outlook.com>
2022-07-13 18:47:00 -0300

    feat: adicionando funcionalidade menuDropDown

--- src/scripts/homepage.scripts.js
+++ src/scripts/homepage.scripts.js
@@ -1,6 +1,8 @@
 import Homepage from "../controller/homepage.controller.js";
+import MenuDropDown from "../controller/menuDropDown.controller.js";
 
 Homepage.createHabitCard();
 
 Homepage.renderUser(Homepage.getUser);
 
+MenuDropDown.viewMenu()
