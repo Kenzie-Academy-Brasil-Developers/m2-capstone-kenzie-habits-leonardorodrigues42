271fd2f15790b04bd0fffd7dfe7e6a449bd23d26
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-07-14 12:02:40 -0300

    fix: Arrumei o problema que não estava fechando o modal

--- src/controller/menuDropDown.controller.js
+++ src/controller/menuDropDown.controller.js
@@ -1,91 +1,97 @@
-import Api from "../models/Api.models.js"
-import Modal from "../models/modal.models.js"
-import Homepage from "./homepage.controller.js"
+import Api from "../models/Api.models.js";
+import Modal from "../models/modal.models.js";
+import Homepage from "./homepage.controller.js";
 
 export default class MenuDropDown {
+  static iconImage = document.querySelector(".iconImage");
 
-    static iconImage = document.querySelector(".iconImage")
+  static async viewMenu() {
+    this.iconImage.addEventListener("click", async (e) => {
+      Modal.logoutIsEditProfile();
 
-    static async viewMenu() {
+      const btnEdit = document.querySelector(".modalLogoutBtnEdit");
+      const btnLogout = document.querySelector(".modalLogoutBtnLogout");
 
-        this.iconImage.addEventListener("click", async (e) => {
-            Modal.logoutIsEditProfile()
+      btnEdit.addEventListener("click", async () => {
+        Modal.editProfile();
 
-            const btnEdit = document.querySelector(".modalLogoutBtnEdit")
-            const btnLogout = document.querySelector(".modalLogoutBtnLogout")
-            
-            btnEdit.addEventListener("click", async () => {
-                Modal.editProfile()
-                
-                const form = document.querySelector(".modalFormEditProfile")
-                const btnSave = document.querySelector(".modalBtnEditProfile")
-                const btnSair = document.querySelector(".modalBtnEditProfileExit")
+        const form = document.querySelector(".modalFormEditProfile");
+        const btnSave = document.querySelector(".modalBtnEditProfile");
+        const btnSair = document.querySelector(".modalBtnEditProfileExit");
 
-                btnSair.addEventListener("click", () => {
-                    
-                })
-                
-                let formElements = [...form.elements]
+        btnSair.addEventListener("click", () => {});
 
-                const user = Homepage.getUser
-                
-                console.log(user.usr_name)
-                console.log(user.usr_image)
-                formElements[0].value = user.usr_name
-                formElements[1].value = user.usr_image
+        let formElements = [...form.elements];
 
-                btnSave.addEventListener("click", async (event) =>{
-                    const inputNameEditProfile = document.querySelector(".inputNameEditProfile");
-                    const inputUrlEditProfile = document.querySelector(".inputUrlEditProfile");
-                    const inputNameEditProfileError = document.querySelector(".inputNameEditProfileError");
-                    const inputUrlEditProfileError = document.querySelector(".inputUrlEditProfileError")
-                   const formElements = [...form.elements]
-                    
-                    const data = {
-                        usr_name: formElements[0].value,
-                        usr_image: formElements[1].value
-                    }
+        const user = Homepage.getUser;
 
-                    if (formElements[0].value === "") {
-                        event.preventDefault();
-                        inputNameEditProfile.style.border = "1.5px solid red";
-                        inputNameEditProfileError.innerText = "Campo obrigatório";
-                        return;
-                    } else {
-                        inputNameEditProfile.style.border = "1.5px solid blue";
-                        inputNameEditProfileError.innerText = "";
-                    }
-                    if (formElements[1].value === "") {
-                        event.preventDefault();
-                        inputUrlEditProfile.style.border = "1.5px solid red";
-                        inputUrlEditProfileError.innerText = "Campo obrigatório";
-                        return;
-                    } else {
-                        inputUrlEditProfile.style.border = "1.5px solid blue";
-                        inputUrlEditProfileError.innerText = "";
-                    }
-            
-                    await Api.uptadeProfile(data)
-                    .then((response) => localStorage.setItem("@kenzie-habits:user", JSON.stringify(response)))
-                    .then(() => location.reload())
-                    .catch((err) => console.log(err))
-                })
-            })
+        console.log(user.usr_name);
+        console.log(user.usr_image);
+        formElements[0].value = user.usr_name;
+        formElements[1].value = user.usr_image;
 
-            btnLogout.addEventListener("click", () => {
-                localStorage.removeItem("@kenzie-habits:user")
-                localStorage.removeItem("@kenzie-habits:token")
+        btnSave.addEventListener("click", async (event) => {
+          const inputNameEditProfile = document.querySelector(
+            ".inputNameEditProfile"
+          );
+          const inputUrlEditProfile = document.querySelector(
+            ".inputUrlEditProfile"
+          );
+          const inputNameEditProfileError = document.querySelector(
+            ".inputNameEditProfileError"
+          );
+          const inputUrlEditProfileError = document.querySelector(
+            ".inputUrlEditProfileError"
+          );
+          const formElements = [...form.elements];
 
-                location.href = "../../index.html"
-            })
-        })
+          const data = {
+            usr_name: formElements[0].value,
+            usr_image: formElements[1].value,
+          };
 
-    }
+          if (formElements[0].value === "") {
+            event.preventDefault();
+            inputNameEditProfile.style.border = "1.5px solid red";
+            inputNameEditProfileError.innerText = "Campo obrigatório";
+            return;
+          } else {
+            inputNameEditProfile.style.border = "1.5px solid blue";
+            inputNameEditProfileError.innerText = "";
+          }
+          if (formElements[1].value === "") {
+            event.preventDefault();
+            inputUrlEditProfile.style.border = "1.5px solid red";
+            inputUrlEditProfileError.innerText = "Campo obrigatório";
+            return;
+          } else {
+            inputUrlEditProfile.style.border = "1.5px solid blue";
+            inputUrlEditProfileError.innerText = "";
+          }
+
+          await Api.uptadeProfile(data)
+            .then((response) =>
+              localStorage.setItem(
+                "@kenzie-habits:user",
+                JSON.stringify(response)
+              )
+            )
+            .then(() => location.reload())
+            .catch((err) => console.log(err));
+        });
+      });
+
+      btnLogout.addEventListener("click", () => {
+        localStorage.removeItem("@kenzie-habits:user");
+        localStorage.removeItem("@kenzie-habits:token");
+
+        location.href = "../../index.html";
+      });
+    });
+  }
 }
 
-
 // iconImage.addEventListener("click", async () => {
-    
 
 //     const form = document.querySelector(".modalFormEditProfile")
 //     let formElements = [...form.elements]
@@ -99,7 +105,7 @@ export default class MenuDropDown {
 
 //     btnEdit.addEventListener("click", async () =>{
 //         formElements = [...form.elements]
-        
+
 //         const data = {
 //             usr_name: formElements[0].value,
 //             usr_image: formElements[1].value
@@ -108,7 +114,7 @@ export default class MenuDropDown {
 //         await Api.uptadeProfile(data)
 //         .then((response) => console.log(response))
 //         .catch((err) => console.log(err))
-        
-//     }) 
+
+//     })
 
 // })
