8f55787efb61ecb140ca96ee650f93e6f532a909
vagme <vagmengali2015@gmail.com>
2022-07-14 00:00:35 -0300

    feat: adicinando animação aos botões

--- src/scripts/homepage.scripts.js
+++ src/scripts/homepage.scripts.js
@@ -1,8 +1,16 @@
 import Homepage from "../controller/homepage.controller.js";
 import MenuDropDown from "../controller/menuDropDown.controller.js";
+import Modal from "../models/modal.models.js";
 
 Homepage.createHabitCard();
 
 Homepage.renderUser(Homepage.getUser);
 
 MenuDropDown.viewMenu()
+
+// Modal.delete()
+// Modal.editHabit()
+// Modal.createHabit()
+//Modal.editProfile()
+// Modal.habitChangedSucess()
+// Modal.habitCreateSucess()
